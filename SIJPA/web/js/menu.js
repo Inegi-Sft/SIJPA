2e8de4d55737362f67ae5c2f2110d5012b30f1e2
Inegi-Sft <cars2987@hotmail.com>
2019-09-26 08:59:53 -0500

    25/09/2019 menu js no sirve aun Carlos

--- SIJPA/web/js/menu.js
+++ SIJPA/web/js/menu.js
@@ -3,69 +3,70 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+window.onload = function () {
+    var menuLeft = document.getElementById('cbp-spmenu-s1'),
+            menuRight = document.getElementById('cbp-spmenu-s2'),
+            menuTop = document.getElementById('cbp-spmenu-s3'),
+            menuBottom = document.getElementById('cbp-spmenu-s4'),
+            showLeft = document.getElementById('showLeft'),
+            showRight = document.getElementById('showRight'),
+            showTop = document.getElementById('showTop'),
+            showBottom = document.getElementById('showBottom'),
+            showLeftPush = document.getElementById('showLeftPush'),
+            showRightPush = document.getElementById('showRightPush'),
+            body = document.body;
 
-var menuLeft = document.getElementById('cbp-spmenu-s1'),
-        menuRight = document.getElementById('cbp-spmenu-s2'),
-        menuTop = document.getElementById('cbp-spmenu-s3'),
-        menuBottom = document.getElementById('cbp-spmenu-s4'),
-        showLeft = document.getElementById('showLeft'),
-        showRight = document.getElementById('showRight'),
-        showTop = document.getElementById('showTop'),
-        showBottom = document.getElementById('showBottom'),
-        showLeftPush = document.getElementById('showLeftPush'),
-        showRightPush = document.getElementById('showRightPush'),
-        body = document.body;
+    document.getElementById('showLeft').onclick = function () {
+        toggle(this, 'active');
+        toggle(menuLeft, 'cbp-spmenu-open');
+        disableOther('showLeft');
+    };
+    showRight.onclick = function () {
+        toggle(this, 'active');
+        toggle(menuRight, 'cbp-spmenu-open');
+        disableOther('showRight');
+    };
+    showTop.onclick = function () {
+        toggle(this, 'active');
+        toggle(menuTop, 'cbp-spmenu-open');
+        disableOther('showTop');
+    };
+    showBottom.onclick = function () {
+        toggle(this, 'active');
+        toggle(menuBottom, 'cbp-spmenu-open');
+        disableOther('showBottom');
+    };
+    showLeftPush.onclick = function () {
+        toggle(this, 'active');
+        toggle(body, 'cbp-spmenu-push-toright');
+        toggle(menuLeft, 'cbp-spmenu-open');
+        disableOther('showLeftPush');
+    };
+    showRightPush.onclick = function () {
+        toggle(this, 'active');
+        toggle(body, 'cbp-spmenu-push-toleft');
+        toggle(menuRight, 'cbp-spmenu-open');
+        disableOther('showRightPush');
+    };
 
-document.getElementById('showLeft').onclick = function () {
-    toggle(this, 'active');
-    toggle(menuLeft, 'cbp-spmenu-open');
-    disableOther('showLeft');
-};
-showRight.onclick = function () {
-    classie.toggle(this, 'active');
-    classie.toggle(menuRight, 'cbp-spmenu-open');
-    disableOther('showRight');
-};
-showTop.onclick = function () {
-    classie.toggle(this, 'active');
-    classie.toggle(menuTop, 'cbp-spmenu-open');
-    disableOther('showTop');
-};
-showBottom.onclick = function () {
-    classie.toggle(this, 'active');
-    classie.toggle(menuBottom, 'cbp-spmenu-open');
-    disableOther('showBottom');
-};
-showLeftPush.onclick = function () {
-    classie.toggle(this, 'active');
-    classie.toggle(body, 'cbp-spmenu-push-toright');
-    classie.toggle(menuLeft, 'cbp-spmenu-open');
-    disableOther('showLeftPush');
-};
-showRightPush.onclick = function () {
-    classie.toggle(this, 'active');
-    classie.toggle(body, 'cbp-spmenu-push-toleft');
-    classie.toggle(menuRight, 'cbp-spmenu-open');
-    disableOther('showRightPush');
-};
-
-function disableOther(button) {
-    if (button !== 'showLeft') {
-        classie.toggle(showLeft, 'disabled');
+    function disableOther(button) {
+        if (button !== 'showLeft') {
+            toggle(showLeft, 'disabled');
+        }
+        if (button !== 'showRight') {
+            toggle(showRight, 'disabled');
+        }
+        if (button !== 'showTop') {
+            toggle(showTop, 'disabled');
+        }
+        if (button !== 'showBottom') {
+            toggle(showBottom, 'disabled');
+        }
+        if (button !== 'showLeftPush') {
+            toggle(showLeftPush, 'disabled');
+        }
+        if (button !== 'showRightPush') {
+            toggle(showRightPush, 'disabled');
+        }
     }
-    if (button !== 'showRight') {
-        classie.toggle(showRight, 'disabled');
-    }
-    if (button !== 'showTop') {
-        classie.toggle(showTop, 'disabled');
-    }
-    if (button !== 'showBottom') {
-        classie.toggle(showBottom, 'disabled');
-    }
-    if (button !== 'showLeftPush') {
-        classie.toggle(showLeftPush, 'disabled');
-    }
-    if (button !== 'showRightPush') {
-        classie.toggle(showRightPush, 'disabled');
-    }
-}
+};

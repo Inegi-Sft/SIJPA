6540cbce18b049ba4a801a5d382a400927d2e44c
Inegi-Sft <55498000+Inegi-Sft@users.noreply.github.com>
2019-10-21 08:55:03 -0500

    Update funciones.js

--- SIJPA/web/js/funciones.js
+++ SIJPA/web/js/funciones.js
@@ -28,8 +28,6 @@ $(document).ready(function () {
 
     //oculta los divs con clase oculto (se utiliza en lugar de nacimiento y residencia)
     $('.oculto').hide();
-    $('.dependiente').val('-2');
-    $('.depenFecha').val('1899-09-09');
   
     /***************************** FUNCIONES JUZGADOS *******************************/
     //select forma de organizacion

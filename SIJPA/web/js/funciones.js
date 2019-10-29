47c6f7c3c457acb356c02626242f1c0c61b02cbc
Inegi-Sft <55498000+Inegi-Sft@users.noreply.github.com>
2019-10-29 12:53:45 -0600

    29/10/2019 cambios en funciones --Carlos

--- SIJPA/web/js/funciones.js
+++ SIJPA/web/js/funciones.js
@@ -37,26 +37,26 @@ $(document).ready(function () {
         switch ($("#fOrganiza").val()) {
             case '1':
                 $("#dRegJudicial").fadeIn("slow");
-                $("#regJudicial").val("");
+                $("#regJudicial").val("").prop("required",true);
                 $("#dDistJudicial,#dPartJudicial").hide();
-                $("#distJudicial,#partJudicial").val("-2");
+                $("#distJudicial,#partJudicial").val("-2").prop("required",false);
                 break;
             case '2':
                 $("#dRegJudicial,#dPartJudicial").hide();
-                $("#regJudicial,#partJudicial").val("-2");
+                $("#regJudicial,#partJudicial").val("-2").prop("required",false);
                 $("#dDistJudicial").fadeIn("slow");
-                $("#distJudicial").val("");
+                $("#distJudicial").val("").prop("required",true);
                 break;
             case '3':
                 $("#dRegJudicial,#dDistJudicial").hide();
-                $("#regJudicial,#distJudicial").val("-2");
+                $("#regJudicial,#distJudicial").val("-2").prop("required",false);
                 $("#dPartJudicial").fadeIn("slow");
-                $("#partJudicial").val("");
+                $("#partJudicial").val("").prop("required",true);
                 break;
         }
         if ($("#fOrganiza").val() > 3 || $("#fOrganiza").val() === "") {
             $("#dRegJudicial,#dDistJudicial,#dPartJudicial").fadeOut("slow");
-            $("#regJudicial,#distJudicial,#partJudicial").val("-2");
+            $("#regJudicial,#distJudicial,#partJudicial").val("-2").prop("required",false);
         }
     });
     /*---------------------------- FIN FUNCIONES JUZGADOS ----------------------------*/
@@ -550,21 +550,3 @@ function llenaNormaT(vNorma) {
     }
 };
 /*---------------------------- FIN FUNCIONES DELITOS --------------------------*/
-      
-/***************************** FUNCIONES GENERALES *********************************/
-// Fechas No identificadas
-/***
- * 
- * @param {type} idChk
- * @param {type} idTxtDate
- * @returns {undefined}
- */
-//function fechaNoIdent(idChk, idTxtDate) {
-//    if ($(idChk).is(":checked")) {
-//        $(idTxtDate).val("1899-09-09");
-//        $(idTxtDate).prop("disabled", true);
-//    } else {
-//        $(idTxtDate).val("");
-//        $(idTxtDate).prop("disabled", false);
-//    }
-//};

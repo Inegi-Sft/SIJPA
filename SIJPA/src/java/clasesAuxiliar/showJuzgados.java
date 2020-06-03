9b831de3a4d12268c81fd911b7670396a44dee7b
ANTONIO.CORIA <ANTONIO.CORIA@GSJPIG049227L.inegi.gob.mx>
2020-06-03 16:58:29 -0500

    03/06/2020  Val  fecha nacimiento correcto --marco coria

--- SIJPA/src/java/clasesAuxiliar/showJuzgados.java
+++ SIJPA/src/java/clasesAuxiliar/showJuzgados.java
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
 public class showJuzgados {
     Conexion_Mysql conn = new Conexion_Mysql();
     ArrayList<String> lista;
-    ArrayList<String[]> listaTabla;
+    ArrayList<String[]> listaTabla,ListaDatosJuz;
     String sql;
     ResultSet rs;
     
@@ -86,4 +86,28 @@ public class showJuzgados {
         }
         return listaTabla;
     }
+    public ArrayList findDatosJuzgado(String juzgadoClave){
+        try {
+            conn.Conectar();
+            ListaDatosJuz = new ArrayList<String[]>();
+            sql = "SELECT *  "
+                    + "FROM DATOS_JUZGADOS_ADOJC WHERE JUZGADO_CLAVE='"+ juzgadoClave +"' "
+                    + "ORDER BY 1";
+            rs = conn.consultar(sql);
+            while (rs.next()) {
+                ListaDatosJuz.add(new String[]{
+                    rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),
+                    rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10),
+                    rs.getString(11),rs.getString(12),rs.getString(13),rs.getString(14),rs.getString(15),rs.getString(16),
+                    rs.getString(17),rs.getString(18),rs.getString(19),rs.getString(20),rs.getString(21),rs.getString(22),
+                    rs.getString(23),rs.getString(24),
+                });
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(showJuzgados.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ListaDatosJuz;
+    }
+    
 }
